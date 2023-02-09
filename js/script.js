c654d2b46dcc24914d6614da7e1fadc0b0ca4099
fracs31 <pist.fracs95@gmail.com>
2023-02-09 16:22:53 +0100

    ADD button to start the game

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
-let row = 10; //numero di righe
-let cells = row ** 2 ; //numero di celle
-let gridElement = document.getElementById("grid"); //griglia nel DOM
+let buttonElement = document.getElementById("btn"); //bottone per iniziare la partita
 
-//Ciclo per la creazione delle celle della griglia
-for (let i = 0; i < cells; i++) {
-    let cellElement = document.createElement("div"); //creazione della cella
-    let value = i + 1; //valore della cella
-    cellElement.setAttribute("class", "cell"); //assegno alla cella una classe
-    cellElement.setAttribute("onClick", "onClick(" + i + ")"); //assegno alla cella una funzione per quando viene cliccata
-    cellElement.style.width = "calc(100% / " + " " + row + ")"; //assegno la larghezza alla cella
-    cellElement.innerHTML = value; //assegno alla cella il valore
-    gridElement.appendChild(cellElement); //aggiungo la cella alla griglia
+buttonElement.addEventListener("click", startGame); //quando viene premuto il bottone il gioco inizia
+
+function startGame() {
+    buttonElement.remove(); //elimino il bottone per iniziare la partita
+    let row = 10; //numero di righe
+    let cells = row ** 2 ; //numero di celle
+    let gridElement = document.getElementById("grid"); //griglia nel DOM
+    //Ciclo per la creazione delle celle della griglia
+    for (let i = 0; i < cells; i++) {
+        let cellElement = document.createElement("div"); //creazione della cella
+        let value = i + 1; //valore della cella
+        cellElement.setAttribute("class", "cell"); //assegno alla cella una classe
+        cellElement.setAttribute("onClick", "onClick(" + i + ")"); //assegno alla cella una funzione per quando viene cliccata
+        cellElement.style.width = "calc(100% / " + " " + row + ")"; //assegno la larghezza alla cella
+        cellElement.innerHTML = value; //assegno alla cella il valore
+        gridElement.appendChild(cellElement); //aggiungo la cella alla griglia
+    }
 }
 
 //Funzione per quando viene cliccata la cella
