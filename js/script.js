bc8f9d455a9b69490b13c50720cca141ef5e17f8
fracs31 <pist.fracs95@gmail.com>
2023-02-09 16:07:15 +0100

    ADD onClick function for the cells

--- js/script.js
+++ js/script.js
@@ -6,8 +6,17 @@ let gridElement = document.getElementById("grid"); //griglia nel DOM
 for (let i = 0; i < cells; i++) {
     let cellElement = document.createElement("div"); //creazione della cella
     let value = i + 1; //valore della cella
-    cellElement.classList.add("cell"); //assegno alla cella una classe
+    cellElement.setAttribute("class", "cell"); //assegno alla cella una classe
+    cellElement.setAttribute("onClick", "onClick(" + i + ")"); //assegno alla cella una funzione per quando viene cliccata
     cellElement.style.width = "calc(100% / " + " " + row + ")"; //assegno la larghezza alla cella
     cellElement.innerHTML = value; //assegno alla cella il valore
     gridElement.appendChild(cellElement); //aggiungo la cella alla griglia
+}
+
+//Funzione per quando viene cliccata la cella
+function onClick(index) {
+    let cellList = document.querySelectorAll(".cell"); //array con tutte le celle
+    let cell = cellList[index]; //cella cliccata
+    console.log("Cella numero: " + (index + 1)); //stampo in console il numero della cella
+    cell.classList.toggle("bg-green"); //cambio la classe alla cella cliccata
 }
