b145787fa19807133cc1ebbcfd737b5b2a03b683
fracs31 <pist.fracs95@gmail.com>
2023-02-10 11:02:26 +0100

    CHANGE the onClick function for the cells

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function startGame() {
         let cellElement = document.createElement("div"); //creazione della cella
         let value = i + 1; //valore della cella
         cellElement.setAttribute("class", "cell"); //assegno alla cella una classe
-        cellElement.setAttribute("onClick", "onClick(" + i + ")"); //assegno alla cella una funzione per quando viene cliccata
+        cellElement.addEventListener("click", onClick); //assegno alla cella una funzione per quando viene cliccata
         cellElement.style.width = "calc(100% / " + " " + row + ")"; //assegno la larghezza alla cella
         cellElement.innerHTML = value; //assegno alla cella il valore
         gridElement.appendChild(cellElement); //aggiungo la cella alla griglia
@@ -20,9 +20,8 @@ function startGame() {
 }
 
 //Funzione per quando viene cliccata la cella
-function onClick(index) {
-    let cellList = document.querySelectorAll(".cell"); //array con tutte le celle
-    let cell = cellList[index]; //cella cliccata
-    console.log("Cella numero: " + (index + 1)); //stampo in console il numero della cella
+function onClick(event) {
+    let cell = event.target; //cella cliccata
+    console.log(cell); //stampo in console la cella cliccata
     cell.classList.toggle("bg-green"); //cambio la classe alla cella cliccata
 }
